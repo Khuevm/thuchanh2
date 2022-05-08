1b99fb554a9264c740a48f6faf880c9e85f4e429
Khuevm <vngminhkhue@gmail.com>
2022-05-08 15:12:55 +0700

    upload project

---
+++ src/main/java/product/Baithuchanh2nhom22Application.java
@@ -0,0 +1,13 @@
+package product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baithuchanh2nhom22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baithuchanh2nhom22Application.class, args);
+	}
+
+}
